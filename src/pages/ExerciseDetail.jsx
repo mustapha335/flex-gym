440afcddfac4f114f332394386e21459c5202315
mustapha335 <reezy77jay@gmail.com>
2022-06-28 12:16:48 +0100

    exercises video completed

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -4,12 +4,12 @@ import { Box } from "@mui/material";
 
 import { exerciseOptions, fetchData, youtubeOptions } from "../utils/fetchData";
 import Detail from "../components/Detail";
-// import ExerciseVideos from '../components/ExerciseVideos';
+import ExerciseVideos from "../components/ExerciseVideos";
 // import SimilarExercises from '../components/SimilarExercises';
 
 const ExerciseDetail = () => {
   const [exerciseDetail, setExerciseDetail] = useState({});
-  // const [exerciseVideos, setExerciseVideos] = useState([]);
+  const [exerciseVideos, setExerciseVideos] = useState([]);
   // const [targetMuscleExercises, setTargetMuscleExercises] = useState([]);
   // const [equipmentExercises, setEquipmentExercises] = useState([]);
   const { id } = useParams();
@@ -28,8 +28,11 @@ const ExerciseDetail = () => {
       );
       setExerciseDetail(exerciseDetailData);
 
-      // const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name} exercise`, youtubeOptions);
-      // setExerciseVideos(exerciseVideosData.contents);
+      const exerciseVideosData = await fetchData(
+        `${youtubeSearchUrl}/search?query=${exerciseDetailData.name} exercise`,
+        youtubeOptions
+      );
+      setExerciseVideos(exerciseVideosData.contents);
 
       // const targetMuscleExercisesData = await fetchData(`${exerciseDbUrl}/exercises/target/${exerciseDetailData.target}`, exerciseOptions);
       // setTargetMuscleExercises(targetMuscleExercisesData);
@@ -46,8 +49,11 @@ const ExerciseDetail = () => {
   return (
     <Box sx={{ mt: { lg: "96px", xs: "60px" } }}>
       <Detail exerciseDetail={exerciseDetail} />
-      {/* <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name} />
-      <SimilarExercises targetMuscleExercises={targetMuscleExercises} equipmentExercises={equipmentExercises} /> */}
+      <ExerciseVideos
+        exerciseVideos={exerciseVideos}
+        name={exerciseDetail.name}
+      />
+      {/* <SimilarExercises targetMuscleExercises={targetMuscleExercises} equipmentExercises={equipmentExercises} /> */}
     </Box>
   );
 };
