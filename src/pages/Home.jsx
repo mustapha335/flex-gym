413b8865a673670f43d3cd882bb764a0fb927fd9
mustapha335 <reezy77jay@gmail.com>
2022-06-28 01:36:16 +0100

    search and category filters completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box } from "@mui/material";
+import HeroBanner from "../components/HeroBanner";
+import SearchExcercises from "../components/SearchExcercises";
+import Excersises from "../components/Excersises";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState("all");
+
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExcercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Excersises
+        setExercises={setExercises}
+        exercises={exercises}
+        bodyPart={bodyPart}
+      />
+    </Box>
+  );
 };
 
 export default Home;
